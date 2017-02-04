4a6229d324e26a76c7edd7ee99886d59233a9db7
Jared Ottley <jared.ottley@alfresco.com>
2017-02-03 17:29:16 -0700

    [SFDCENG-610] getBlob is returning a closed InputStream -override ByteArrayHttpMessageConverter to allow all media types -change extractor to use the byteArray message converter in the RestRemplate -to keep the existing method signature as InputStream return byteArray as byteArrayInputStream -remove default content-type header used in other tests to allow return on non json content.

--- src/main/java/org/springframework/social/salesforce/api/impl/SObjectsTemplate.java
+++ src/main/java/org/springframework/social/salesforce/api/impl/SObjectsTemplate.java
@@ -5,17 +5,16 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
-import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.social.salesforce.api.SObjectDetail;
 import org.springframework.social.salesforce.api.SObjectOperations;
 import org.springframework.social.salesforce.api.SObjectSummary;
 import org.springframework.social.salesforce.api.Salesforce;
 import org.springframework.social.support.URIBuilder;
 import org.springframework.util.StringUtils;
-import org.springframework.web.client.ResponseExtractor;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.IOException;
+import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -69,13 +68,12 @@ public class SObjectsTemplate extends AbstractSalesForceOperations<Salesforce> i
     @Override
     public InputStream getBlob(String name, String id, String field) {
         requireAuthorization();
-        return restTemplate.execute(api.getBaseUrl() + "/{version}/sobjects/{name}/{id}/{field}",
-                HttpMethod.GET, null, new ResponseExtractor<InputStream>() {
-                    @Override
-                    public InputStream extractData(ClientHttpResponse response) throws IOException {
-                        return response.getBody();
-                    }
-                }, API_VERSION, name, id, field);
+        HttpHeaders headers = new HttpHeaders();
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+        ResponseEntity<byte[]> response =  restTemplate.exchange(
+                api.getBaseUrl() + "/{version}/sobjects/{name}/{id}/{field}", HttpMethod.GET, entity, byte[].class,
+                API_VERSION, name, id, field);
+        return new ByteArrayInputStream(response.getBody());
     }
 
     @Override
